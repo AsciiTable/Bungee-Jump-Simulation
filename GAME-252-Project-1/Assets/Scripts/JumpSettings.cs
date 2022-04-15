d82de5aafad28ae937191802dd8d9a70c18583e8
smshields <samuel.m.shields@gmail.com>
2022-04-15 12:07:39 -0700

    Basic Jump Settings Done
    Still going to add an update to make sure it's easier to manage and manipulate

---
+++ GAME-252-Project-1/Assets/Scripts/JumpSettings.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpSettings : MonoBehaviour
+{
+    /** Variables in Simulation: 
+     * 
+     * Height of Jump
+     * Weight of the Friend
+     * Spring Constant
+     * Obstacles in Area
+     * Wind Speed
+     * Unstretched Bungee Cord
+     */
+
+    //Simulation Game Object References
+    public GameObject topPlatform; //Platform Friend is Jumping off of
+    public GameObject friend; //Friend object
+    public Rigidbody friendRb; //Friend rigidbody
+    public SpringJoint bungeeCord; //bungee
+    public WindForce windForce; //wind controls
+    
+
+    //Simulation Settings
+    public float platformHeight = 45f;
+    public float friendMass = 90f;
+    public float springConstant = 90f;
+    public float cordLength = 30f;
+    
+    public Vector3 windDirection = new Vector3(10,0,0);
+    public float windStrength = 150f;
+    public float timeBetweenBlows = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Set Platform Height and Friend height simultaneously
+        topPlatform.transform.position = new Vector3(topPlatform.transform.position.x, platformHeight, topPlatform.transform.position.z);
+        friend.transform.position = new Vector3(friend.transform.position.x, platformHeight, friend.transform.position.z);
+        //Set friend Mass
+        friendRb.mass = friendMass;
+        //Set spring strength
+        bungeeCord.spring = springConstant;
+        //Set unstretched cord length
+        bungeeCord.maxDistance = cordLength;
+        //Set Wind settings
+        windForce.windDirection = windDirection;
+        windForce.windStrength = windStrength;
+        windForce.timeBetweenBlows = timeBetweenBlows;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
